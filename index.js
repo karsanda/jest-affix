e3a52c7995f3972a9289884ce15f386451377e53
Pinto Karsanda <pinto.karsanda@vidio.com>
2020-05-15 22:42:44 +0700

    affix for tags, id, and class

--- index.js
+++ index.js
@@ -1,4 +1,58 @@
 function affix(selector) {
-  return selector
+  let selectorString = selector || ''
+  const nodes = []
+  const match = /[#\.\(\{a-zA-Z]/.exec(selector);
+
+  if (!match) return;
+
+  switch(match[0]) {
+    case '#':
+    case '.':
+      nodes.push(document.createElement('div'))
+      break
+    
+    default:
+      const tag = /[a-zA-Z0-9]+/.exec(selector);
+      selectorString = selectorString.substring(tag.index + tag[0].length);
+
+      nodes.push(document.createElement(tag[0]));
+  }
+
+  addAttributes(nodes, selectorString)
+
+  for(const node of nodes) {
+    document.body.appendChild(node)
+  }
 }
+
+function addAttributes(nodes, selectorString) {
+  const match = /[\>\+\^\*#\.\[\{\s]/.exec(selectorString)
+  
+  if (!match) return;
+
+  switch (match[0]) {
+    case '#':
+      const ids = /[a-zA-Z0-9_\-]+/.exec(selectorString)
+      if (!ids) break
+      
+      for (const node of nodes) {
+        node.setAttribute('id', ids[0])
+      }
+      
+      addAttributes(nodes, selectorString.substring(ids.index + ids[0].length))
+      break
+      
+    case '.':
+      const classes = /[a-zA-Z0-9_\-]+/.exec(selectorString)
+      if (!classes) break
+      
+      for (const node of nodes) {
+        node.setAttribute('class', ((node.getAttribute('class')) ? node.getAttribute('class') + ' ' : '') + classes[0])
+      }
+      
+      addAttributes(nodes, selectorString.substring(classes.index + classes[0].length))
+      break
+  }
+}
+
 module.exports = affix;
