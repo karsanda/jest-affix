d1afb769ee9932ac56c8f693794e9de83fba2bbc
Pinto Karsanda <pinto.karsanda@vidio.com>
2020-05-16 12:41:49 +0700

    affix for child nodes (e.g. div span li, div > span > li)

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function affix(selector) {
   let selectorString = selector || ''
   const nodes = []
-  const match = /[#\.\(\{a-zA-Z]/.exec(selector);
-
+  const match = /[#\.(\s|\s\>\s|\>)\(\{a-zA-Z]/.exec(selector);
   if (!match) return;
 
   switch(match[0]) {
@@ -10,7 +9,16 @@ function affix(selector) {
     case '.':
       nodes.push(document.createElement('div'))
       break
-    
+
+    case ' ':
+      selectorString = selectorString.substring(match.index + match[0].length);
+      const childrenTag = /$#|\./.exec(selectorString);
+
+      if (childrenTag) {
+        nodes.push(document.createElement('div'))
+        break
+      }
+
     default:
       const tag = /[a-zA-Z0-9]+/.exec(selector);
       selectorString = selectorString.substring(tag.index + tag[0].length);
@@ -18,40 +26,53 @@ function affix(selector) {
       nodes.push(document.createElement(tag[0]));
   }
 
-  addAttributes(nodes, selectorString)
+  modify(nodes, selectorString)
 
   for(const node of nodes) {
     document.body.appendChild(node)
   }
+
+  return nodes
 }
 
-function addAttributes(nodes, selectorString) {
-  const match = /[\>\+\^\*#\.\[\{\s]/.exec(selectorString)
-  
+function modify(nodes, selectorString) {
+  let attributesString = selectorString || ''
+  let childNodes = null
+  const match = /[(\s\>\s|\s|\>)\+\^#\.\[\{]/.exec(attributesString)
   if (!match) return;
 
   switch (match[0]) {
     case '#':
-      const ids = /[a-zA-Z0-9_\-]+/.exec(selectorString)
+      const ids = /[a-zA-Z0-9_\-]+/.exec(attributesString)
       if (!ids) break
       
       for (const node of nodes) {
         node.setAttribute('id', ids[0])
       }
       
-      addAttributes(nodes, selectorString.substring(ids.index + ids[0].length))
+      modify(nodes, attributesString.substring(ids.index + ids[0].length))
       break
       
     case '.':
-      const classes = /[a-zA-Z0-9_\-]+/.exec(selectorString)
+      const classes = /[a-zA-Z0-9_\-]+/.exec(attributesString)
       if (!classes) break
       
       for (const node of nodes) {
         node.setAttribute('class', ((node.getAttribute('class')) ? node.getAttribute('class') + ' ' : '') + classes[0])
       }
       
-      addAttributes(nodes, selectorString.substring(classes.index + classes[0].length))
+      modify(nodes, attributesString.substring(classes.index + classes[0].length))
       break
+    
+    case ' > ':
+    case '>':
+    case ' ':
+      childNodes = affix(attributesString)
+
+      for (const node of nodes) {
+        node.appendChild(childNodes[0])
+      }
+      break;
   }
 }
 
